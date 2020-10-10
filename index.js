98ef2c6589aedeac689fb5b17450bcf43aa5d435
Rajat Lohia <rajatlohia1108@gmail.com>
2020-10-10 19:38:51 +0530

    added home controller

---
+++ index.js
@@ -0,0 +1,19 @@
+const express= require('express');
+const app=express();
+
+const port=8000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/',require('./routes'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.listen(port, function(err){
+    if(err)
+    {
+        console.log(`error in running the server ${err}`);
+    }
+    console.log(`server is running on the port: ${port}`);
+})
